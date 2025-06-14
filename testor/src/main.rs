23a414aa758137f7f40fe17ba04121d75fba1419
wennhmann <info@wennhmann.de>
2025-06-14 15:13:44 +0200

    progress bar

--- testor/src/main.rs
+++ testor/src/main.rs
@@ -1,12 +1,21 @@
 use std::{time::Duration, thread::sleep};
-fn calculation(_n:&i32){
+const CLEAR: &str = "\x1B[2J\x1B[1;1H";
+fn calculation(){
     sleep(Duration::from_secs(1));
-    println!("more and more");
+    println!("abc");
+    }
 
-}
+fn progress<T>(v:Vec<T>,f:fn()){
+    let mut pro = 1;
+   for n in  v.iter(){
+    println!("{}{}",CLEAR,"*".repeat(pro));
+    pro +=1;
+    f();
+
+}}
 fn main() {
-   let vector = vec![1,2,3,4,5,6,7];
-   for n in  vector.iter(){
-    calculation(n);
-   }
+   let vector = vec![1,2,3,4,5,6,7,8,9,10];
+   
+    progress(vector, calculation);
+   
 }
